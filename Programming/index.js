b4a6c600342036a4cf37ea36563480ca9b0c1b1a
Saiprakash7337 <124245331+Saiprakash7337@users.noreply.github.com>
2023-02-27 12:53:25 +0530

    Create index.js

---
+++ Programming/index.js
@@ -0,0 +1,13 @@
+const User = require("./login.js").default;
+
+function validateUser() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const result = User.validate(username, password);
+    if (result === "Login Successful") {
+      alert("Login Successful");
+    } else {
+      alert("Unauthorized access");
+    }
+  }
+  
